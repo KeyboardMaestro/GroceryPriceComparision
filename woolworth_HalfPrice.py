dc23a708741c69ded8ae0240634d226eb3454c44
judepark <psw0616a@naver.com>
2023-02-13 13:41:21 +1100

    Fixed "/" file directory error

--- woolworth_HalfPrice.py
+++ woolworth_HalfPrice.py
@@ -18,7 +18,7 @@ browser = webdriver.Chrome(options=options)
 #browser.find_element()
 attributes = ["Date", "Price"]
 index = 0
-for page in range(1, 38):
+for page in range(1, 56):
     print("page : " + str(page))
     target_url = ("https://www.woolworths.com.au/shop/browse/specials/half-price?icmpid=sm-hp-hero2:half-price-specials%7Cwk33&pageNumber=" + str(page))
     browser.get(target_url)
@@ -28,6 +28,7 @@ for page in range(1, 38):
     for item in items:
         index += 1
         title = item.find_next("a", attrs={"class":"product-title-link"}).get_text()
+        title = title.replace("/", ",")
         price_check = item.find("div", attrs={"class" : "primary"})
         out_of_stock = item.find("div", attrs={"class":"product-tile-unavailable-tag ng-star-inserted"})
         if price_check:
